4e88a16362914ca93c2644f56a5eedc25e8b1d59
Your Name <you@example.com>
2016-05-23 17:53:01 -0700

    Updated Filesystem PhP

--- src/Destinations/Filesystem.php
+++ src/Destinations/Filesystem.php
@@ -3,14 +3,26 @@
 namespace Logger\Destinations;
 
 class Filesystem implements DestinationInterface {
+
+	private $file = "blacklight.log";
+
+	public function __construct ($file, array $data){
+
+		$data = [
+			'name' => $data['Earnest'],
+            'worked' => $data['Yes']
+		]
+		
+		$this->file = $file;
+		$this->data = $data;
+
+		file_put_contents($file, $data . "\n", FILE_APPEND);
+	}
+
 	public function save(array $data) {
 	   	// Save data to a file, appending it to a newline. Create the file if it doesn't exist.
 	   	// Note: $data is a string — it's already been run through your Formatter->format function
 
-	   	$this->file = $file;
-
-	   	$this->data = $data;
-
-	   	file_put_contents($file, $data . "\n", FILE_APPEND);
+	   	
 	}
 }
